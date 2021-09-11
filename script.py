bc2ebdc9312eb454d6e48f66b6490e5036dce229
ritikchamola <ritikchamola@gmail.com>
2021-09-11 19:49:23 +0000

    Add qt5 example

---
+++ script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from PyQt5.QtWidgets import *
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec_()
